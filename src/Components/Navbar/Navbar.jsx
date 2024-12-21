e8a91e06d73806648da1d0668622c892e34e52a2
Hind_Jammar <121793980+hindjammar@users.noreply.github.com>
2024-12-21 16:45:22 +0100

    Some changes in Folder navbar and LoginSignup

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -53,9 +53,12 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="nav-login-cart">
-        <Link to="/login">
+        {localStorage.getItem('auth-token')
+        ? <button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+        :<Link to="/login">
           <button>Login</button>
-        </Link>
+        </Link>}
+        
         <Link to="/cart">
           <div className="nav-cart">
             <img src={cart_icon} alt="Cart Icon" />
